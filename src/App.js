eed33bbe8d5b30eef6b2f295a1974a10d90ecae4
unknown <mikeitt009@gmail.com>
2019-09-30 16:12:55 -0400

    connected register component to firebase authentication

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import firebase from "firebase";
 import "./App.css";
 import RegisterUser from "./components/RegisterUser";
+import { API_KEY, AUTH_DOMAIN, DB_URL, PROJECT_ID } from "./config";
 
 function App() {
+  useEffect(() => {
+    const init = {
+      apiKey: API_KEY,
+      authDomain: AUTH_DOMAIN,
+      databaseURL: DB_URL,
+      projectId: PROJECT_ID
+    };
+
+    firebase.initializeApp(init);
+  }, []);
   return (
     <div className="App">
       <RegisterUser />
--- src/components/RegisterUser.js
+++ src/components/RegisterUser.js
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import firebase from "firebase";
 import img from "../images/login-image.jpg";
 
 export default function RegisterUser() {
+  const [input, setInput] = useState({ name: "", email: "", password: "" });
+  const [uid, setUid] = useState("");
+  const [error, setError] = useState("");
+
+  const registerUser = async e => {
+    e.preventDefault();
+
+    const { email, password } = input;
+
+    try {
+      const userObject = await firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password);
+      setInput({ name: "", email: "", password: "" });
+      setUid(userObject.user.uid);
+    } catch (err) {
+      if (err.code === "auth/email-already-in-use") {
+        setError(err.message);
+      }
+
+      if (err.code === "auth/invalid-email") {
+        setError(err.message);
+      }
+      console.log(process.env.REACT_APP_API_KEY);
+    }
+  };
+
   return (
     <div
       style={{ display: "flex", height: "100vh", backgroundColor: "#F8F8F8" }}
     >
       <img src={img} alt="he road." />
       <div style={{ width: "100%", margin: "auto auto" }}>
+        {error ? <p>{error}</p> : null}
         <h1>Register an account</h1>
         <form
           style={{
@@ -21,18 +50,32 @@ export default function RegisterUser() {
             type="text"
             style={styles.inputStyle}
             placeholder="Enter Name"
+            value={input.name}
+            onChange={e => {
+              setInput({ ...input, name: e.target.value });
+            }}
           />
           <input
             type="email"
             style={styles.inputStyle}
             placeholder="Enter Email"
+            value={input.email}
+            onChange={e => {
+              setInput({ ...input, email: e.target.value });
+            }}
           />
           <input
             type="password"
             style={styles.inputStyle}
             placeholder="Enter Password"
+            value={input.password}
+            onChange={e => {
+              setInput({ ...input, password: e.target.value });
+            }}
           />
-          <button style={styles.buttonStyle}>Submit</button>
+          <button style={styles.buttonStyle} onClick={registerUser}>
+            Submit
+          </button>
           <p>
             Have an account already? <strong>Sign In</strong>
           </p>
